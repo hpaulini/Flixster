30c309e3a52f18ea53766b6bd20892c9b6c00491
Helen Paulini <hpaulini@fb.com>
2020-06-25 16:41:28 -0400

    created new activity to display movie details when a movie is selected

--- app/src/main/java/com/helenpaulini/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/helenpaulini/flixster/MovieDetailsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.helenpaulini.flixster.models.Movie;
 
@@ -12,6 +14,9 @@ import org.parceler.Parcels;
 public class MovieDetailsActivity extends AppCompatActivity {
 
     Movie movie;
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,18 @@ public class MovieDetailsActivity extends AppCompatActivity {
         // unwrap the movie passed in via intent, using its simple name as a key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+
+        // resolve the view objects
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvOverview = (TextView) findViewById(R.id.tvOverview);
+        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+
+        // set the title and overview
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+
+        // vote average is 0..10, convert to 0..5 by dividing by 2
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage = voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
     }
 }
