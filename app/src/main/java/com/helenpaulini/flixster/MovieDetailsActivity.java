f1478ba1005bec9da03c9984d608d85124b5c037
Helen Paulini <hpaulini@fb.com>
2020-06-25 16:31:04 -0400

    placeholder image appears while loading

---
+++ app/src/main/java/com/helenpaulini/flixster/MovieDetailsActivity.java
@@ -0,0 +1,24 @@
+package com.helenpaulini.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.helenpaulini.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    Movie movie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+        // unwrap the movie passed in via intent, using its simple name as a key
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+    }
+}
--- app/src/main/java/com/helenpaulini/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/helenpaulini/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.helenpaulini.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,8 +14,11 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.helenpaulini.flixster.MovieDetailsActivity;
 import com.helenpaulini.flixster.R;
 import com.helenpaulini.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -53,7 +57,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         return movies.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView tvTitle;
         TextView tvOverview;
@@ -64,6 +68,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            itemView.setOnClickListener(this);
         }
 
         public void bind(Movie movie) {
@@ -74,13 +79,29 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
                 // then imageURL = backdrop image
                 imageURL = movie.getBackdropPath();
+                Glide.with(context).load(imageURL).placeholder(R.drawable.flicks_backdrop_placeholder).into(ivPoster);
             } else{
-                // else umageURL = poster image
+                // else imageURL = poster image
                 imageURL = movie.getPosterPath();
+                Glide.with(context).load(imageURL).placeholder(R.drawable.flicks_movie_placeholder).into(ivPoster);
             }
+        }
 
-            Glide.with(context).load(imageURL).into(ivPoster);
-
+        @Override
+        public void onClick(View v) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the movie at the position, this won't work if the class is static
+                Movie movie = movies.get(position);
+                // create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // show the activity
+                context.startActivity(intent);
+            }
         }
     }
 }
